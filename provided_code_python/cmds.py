c54c64da8d67fd65efbf83270d1345745a605c94
Jared Dyreson <jared.dyreson@gmail.com>
2021-11-20 12:29:50 -0800

    cleaned house

--- provided_code_python/cmds.py
+++
@@ -1,8 +0,0 @@
-# *********************************************************************
-# This file illustrates how to execute a command and get it's output
-# *********************************************************************
-import commands
-
-# Run ls command, get output, and print it
-for line in commands.getstatusoutput('ls -l'):
-	print line
